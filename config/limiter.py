341e9005ac4b70a208a7d3b6c2abb328af5b600c
viggy3 <hari.v.personal@outlook.com>
2026-02-25 16:31:40 +0000

    bypass loadbalancing ip

--- config/limiter.py
+++ config/limiter.py
@@ -1,5 +1,12 @@
+from fastapi import Request
 from slowapi import Limiter, _rate_limit_exceeded_handler
 from slowapi.util import get_remote_address
 from slowapi.errors import RateLimitExceeded
 
-limiter = Limiter(key_func=get_remote_address)
+def get_real_ip(request: Request) -> str:
+    forwarded_for = request.headers.get("X-Forwarded-For")
+    if forwarded_for:
+        return forwarded_for.split(",")[0].strip()  # first IP in the chain is the real client
+    return request.client.host  # fallback if header missing
+
+limiter = Limiter(key_func=get_real_ip)
